5f8815e2da5db4f23755c829cab71d1eacc6b0b9
Knowledgesearcher1 <124164269+Knowledgesearcher1@users.noreply.github.com>
2023-11-03 12:17:01 +0000

    Add files via upload

--- main.py
+++ main.py
@@ -1,31 +1,45 @@
-import sys 
 import pygame
-
+from pygame.sprite import Group
+import game_functions as gf
 from settings import Settings 
+from ship import Ship
+from stars import Stars
+from game_status import GameStats 
+from button import Button
+from scoredoard import Scoreboard
+
 
 def run_game():
-    # Initailize the game 
     pygame.init()
-    
     settings = Settings()
-    # create screen object 
     screen = pygame.display.set_mode(
         (settings.screen_width, settings.screen_height))
     pygame.display.set_caption("Cosmic Defenders")
 
-    # Start the main loop for the game 
+    # Create the stars 
+    stars = Stars(screen, settings)
+    # Creating a ship object 
+    ship = Ship(screen, settings)
+    # Create a group to store bullets in 
+    bullets = Group()
+    # Create a fleet of enemies
+    enemies = Group()
+    gf.create_fleet(settings, screen, ship,  enemies)
+
+    status = GameStats(settings)
+    sb = Scoreboard(settings, screen, status)
+    play_button = Button(settings, screen, "Play")
+
+    # Main game loop
     while True:
 
-        # Watch keybaord and mouse events
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sys.exit()
-
+        gf.check_events(settings, screen, status, sb, play_button, ship, enemies, bullets)
+        stars.update()
+        if status.game_active:
+            ship.update()
+            gf.update_bullets(settings, screen, status, sb, ship, enemies, bullets)
+            gf.update_enemies(settings, status, sb, screen, ship, enemies, bullets)
         
-        # Redraw the screen during each pass through the loop 
-        screen.fill(settings.bg_color)
+        gf.update_screen(screen, stars, sb, ship, enemies, bullets, play_button, status)
         
-        # Make the most recently drawn screen visible
-        pygame.display.flip()
-
 run_game()
